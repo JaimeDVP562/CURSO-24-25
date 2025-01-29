aaaa8f869187128d00177bfa54afd00c2788a351
Jaime <jgavtor562@g.educaand.es>
2025-01-29 21:45:59 +0100

    asdf

--- 01- Base de datos/basesDeDatos/entrega2.sql
+++ 01- Base de datos/basesDeDatos/entrega2.sql
@@ -1,24 +1,25 @@
---Jaime Gavilán Torrero
+--Jaime Gavilï¿½n Torrero
 -- Turno de tarde DAW
 --CURSO 2024/2025
 
 USE jardineria;
 
 
---Ejercicio 01: Muestra un listado con la ciudad y el teléfono de las oficinas de España.
+--Ejercicio 01: Muestra un listado con la ciudad y el telï¿½fono de las oficinas de Espaï¿½a.
 
 SELECT ciudad, telefono
 FROM oficina
-WHERE pais='España';
+WHERE pais='EspaÃ±a';
 
---Ejercicio 02: Muestra un listado con el nombre, apellidos y puesto de aquellos empleados que no sean representantes de ventas.
+--Ejercicio 02: Muestra un listado con el nombre, apellidos y puesto de aquellos empleados que 
+--no sean representantes de ventas.
 
 SELECT nombre, apellido1, apellido2, puesto
 FROM empleado
 WHERE puesto NOT LIKE 'Representante%';
 
---Ejercicio 03: Muestra un listado con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos 
---cuya fecha de entrega ha sido al menos dos días antes de la fecha esperada. Investiga para ello la función DATEDIFF de SQL Server.
+--Ejercicio 03: Muestra un listado con el cï¿½digo de pedido, cï¿½digo de cliente, fecha esperada y fecha de entrega de los pedidos 
+--cuya fecha de entrega ha sido al menos dos dï¿½as antes de la fecha esperada. Investiga para ello la funciï¿½n DATEDIFF de SQL Server.
 
 SELECT codigo_pedido, codigo_cliente, fecha_esperada, fecha_entrega 
 FROM pedido
@@ -30,10 +31,10 @@ WHERE DATEDIFF(DAY, fecha_entrega, fecha_esperada) >= 2;
 SELECT *
 FROM pedido
 WHERE estado = 'Rechazado' AND
-fecha_pedido BETWEEN '2009-01-01' AND '2009-12-31';
+fecha_pedido BETWEEN '2009-01-01' AND '2010-01-01';
 
---Ejercicio 05: Muestra un listado con todos los productos que pertenecen a la gama Ornamentales y que tienen más de 100 unidades 
---en stock. El listado deberá estar ordenado por su precio de venta, mostrando en primer lugar los de mayor precio.
+--Ejercicio 05: Muestra un listado con todos los productos que pertenecen a la gama Ornamentales y que tienen mÃ¡s de 100 unidades 
+--en stock. El listado deberÃ­a estar ordenado por su precio de venta, mostrando en primer lugar los de mayor precio.
 
 SELECT * 
 FROM producto
@@ -42,7 +43,7 @@ AND cantidad_en_stock > 100
 ORDER BY precio_venta DESC;
 
 --Ejercicio 06: Muestra un listado con todos los clientes que sean de la ciudad de Madrid y cuyo representante de ventas tenga el 
---código de empleado 11 o 30.
+--cï¿½digo de empleado 11 o 30.
 
 SELECT 
 	cliente.nombre_cliente,
@@ -53,14 +54,102 @@ FROM cliente
 INNER JOIN empleado ON codigo_empleado = codigo_empleado_rep_ventas
 WHERE ciudad ='Madrid' AND codigo_empleado_rep_ventas = 11 OR codigo_empleado_rep_ventas = 30;
 
---Ejercicio 07: Muestra el nombre de los clientes que no hayan realizado pagos junto con el nombre de sus representantes de ventas.
+
+--Ejercicio 07: Muestra un listado con el nombre de los clientes y el nombre completo de sus representantes junto con la ciudad de 
+--la oficina a la que pertenece el representante.
+
+
+
+------------------------------------------------------------------------------------->Preguntar a Fran<------------------------------------------------
+SELECT 
+	cliente.nombre_cliente AS nombre_cliente,
+	CONCAT(empleado.nombre, ' ', empleado.apellido1, ' ', empleado.apellido2) AS nombre_completo_empleado,
+	oficina.ciudad AS ciudad_oficina
+FROM cliente
+INNER JOIN empleado ON  codigo_empleado_rep_ventas = codigo_empleado 
+INNER JOIN oficina ON empleado.codigo_oficina = oficina.codigo_oficina;
+------------------------------------------------------------------------------------->Preguntar a Fran<------------------------------------------------
+
+
+--Ejercicio 08: Muestra un listado con el nombre de los clientes a los que no se les ha entregado a tiempo un pedido.
 
 SELECT 
 	cliente.nombre_cliente,
-	empleado.nombre,
-	empleado.apellido1,
-	empleado.apellido2
+	pedido.fecha_entrega,
+	pedido.fecha_esperada
 FROM cliente
-INNER JOIN empleado ON codigo_empleado = codigo_empleado_rep_ventas
-RIGTH
-WHERE ciudad ='Madrid' AND codigo_empleado_rep_ventas = 11 OR codigo_empleado_rep_ventas = 30;
+INNER JOIN pedido ON cliente.codigo_cliente = pedido.codigo_cliente
+WHERE pedido.fecha_entrega > pedido.fecha_esperada;
+
+
+--Ejercicio 09: Muestra la direcciÃ³n de las oficinas que tengan clientes en Fuenlabrada.
+SELECT
+	oficina.linea_direccion1,
+	oficina.linea_direccion2,
+	oficina.ciudad
+FROM oficina
+INNER JOIN empleado ON oficina.codigo_oficina = empleado.codigo_oficina
+INNER JOIN cliente ON cliente.codigo_empleado_rep_ventas = empleado.codigo_empleado
+WHERE oficina.ciudad = 'Madrid';
+
+
+--Ejercicio 10: Muestra un listado con las diferentes gamas de producto que ha comprado cada cliente.
+
+
+------------------------------------------------------------------------------------->Preguntar a Fran<------------------------------------------------
+SELECT 
+	cliente.nombre_cliente AS nombre_cliente,
+	producto.gama
+FROM cliente
+INNER JOIN pedido ON pedido.codigo_cliente = cliente.codigo_cliente
+INNER JOIN linea_pedido ON pedido.codigo_pedido = linea_pedido.codigo_pedido
+INNER JOIN producto ON producto.codigo_producto = linea_pedido.codigo_producto
+INNER JOIN gama_producto ON producto.gama = gama_producto.gama;
+------------------------------------------------------------------------------------->Preguntar a Fran<------------------------------------------------
+
+
+--Ejercicio 11: Muestra un listado con las oficinas donde trabajan los empleados que hayan sido los representantes 
+--de ventas de algÃºn cliente que haya realizado la compra de algÃºn producto de la gama Frutales.
+
+
+------------------------------------------------------------------------------------->Preguntar a Fran<------------------------------------------------
+SELECT 
+	oficina.codigo_oficina AS codico_oficina,
+	oficina.ciudad AS ciudad,
+	oficina.pais AS pais
+
+FROM oficina
+INNER JOIN empleado ON empleado.codigo_oficina = oficina.codigo_oficina
+INNER JOIN cliente ON cliente.codigo_empleado_rep_ventas = empleado.codigo_empleado
+INNER JOIN pedido ON pedido.codigo_cliente = cliente.codigo_cliente
+INNER JOIN linea_pedido ON pedido.codigo_pedido = linea_pedido.codigo_pedido
+INNER JOIN producto ON producto.codigo_producto = linea_pedido.codigo_producto
+INNER JOIN gama_producto ON producto.gama = gama_producto.gama
+WHERE gama_producto.gama LIKE 'Frutales' ;
+
+------------------------------------------------------------------------------------->Preguntar a Fran<------------------------------------------------
+
+
+--Ejercicio 12: Muestra un listado que muestre solamente los empleados que no tienen un cliente asociado.
+
+
+------------------------------------------------------------------------------------->Preguntar a Fran<------------------------------------------------
+SELECT DISTINCT *
+FROM empleado
+LEFT JOIN cliente ON empleado.codigo_empleado = cliente.codigo_empleado_rep_ventas;
+------------------------------------------------------------------------------------->Preguntar a Fran<------------------------------------------------
+
+
+--Ejercicio 13: Muestra un listado con el nombre de cada empleado, el nombre de su jefe y el nombre del jefe de su 
+--jefe. Solo deberÃ¡s mostrar 3 columnas, una con el nombre completo del empleado, otra con el nombre completo del jefe 
+--y otra con el nombre completo del jefe del jefe.
+
+------------------------------------------------------------------------------------->Preguntar a Fran<------------------------------------------------
+SELECT 
+	empleado.nombre AS nombre_empleado,
+	jefe.nombre AS nombre_jefe,
+	jefejefe.nombre AS jefe_jefe
+FROM empleado
+INNER JOIN  empleado jefe ON empleado.codigo_empleado = jefe.codigo_empleado
+INNER JOIN  empleado jefejefe ON jefe.codigo_empleado = jefejefe.codigo_empleado;
+------------------------------------------------------------------------------------->Preguntar a Fran<------------------------------------------------
---
+++ 03- Lenguajes-de-marcas/examenUD03Javascript/examenUD03GavilanTorreroJaime/js/javascript.js
@@ -0,0 +1,9 @@
+// Funcion para jugar
+function jugar() {
+  let opcionJugador =
+    parseInt(document.getElementById("Piedra" || "Papel" || "Tijera" || "Lagarto" || "Spock").value);
+
+  //let n2 = parseInt(document.getElementById("numero2").value);
+  document.getElementById("resultado").innerText = "La suma es " + (n1 );
+  //document.getElementById("resultado").className = "resultadoSuma";
+}
