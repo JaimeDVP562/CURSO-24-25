30e0a9c65e93e2cd76451527f206f0aa03d88470
Jaime <jgavtor562@g.educaand.es>
2025-02-01 22:09:17 +0100

    Piedra, Papel, Tijera, Lagarto, Spock

--- 03- Lenguajes-de-marcas/examenUD03Javascript/examenUD03GavilanTorreroJaime/js/javascript.js
+++ 03- Lenguajes-de-marcas/examenUD03Javascript/examenUD03GavilanTorreroJaime/js/javascript.js
@@ -1,6 +1,6 @@
 // Declaramos las variales
 let turnoJugador = document.getElementById('turnoJugador').value; // Turno del jugador  
-let turnoMaquina; // Turno de la maquina
+let turnoMaquina = null; // Turno de la maquina
 
 // Variables para las acciones de cada elemento
 const piedra = ' aplasta ';
@@ -19,7 +19,7 @@ const ganaJugador = ' ¡Ganas tú!';
 
 
 // Array que guarda las opciones posibles de la máquina
-let opciones = ["Piedra", "Papel", "Tijera", "Lagarto", "Spock"]
+let opciones = ["Piedra", "Papel", "Tijera", "Lagarto", "Spock"];
 
 // Función para jugar
 function jugar() {
@@ -43,108 +43,110 @@ function jugar() {
 // Función para comprobar el resultado 
 function comprobarResultado() {
 
-    // Comprobamos si gana el jugador
+    // Comprobamos sacando la piedra
     if (
-        (turnoJugador === "Piedra" && turnoMaquina === "Lagarto")) {
-        document.getElementById('resultado2').innerText = turnoJugador + piedra + turnoMaquina + ganaJugador;
-        document.getElementById('resultado2').className = 'resultadoGanador';
-
+        (turnoJugador === "Piedra" && turnoMaquina === "Papel")) {
+        document.getElementById('resultado2').innerText = turnoMaquina + papel1 + turnoJugador + ganaMaquina;
+        document.getElementById('resultado2').className = 'resultadoPerdedor';
     } else if (
         (turnoJugador === "Piedra" && turnoMaquina === "Tijera")) {
         document.getElementById('resultado2').innerText = turnoJugador + piedra + turnoMaquina + ganaJugador;
         document.getElementById('resultado2').className = 'resultadoGanador';
     }
-    // Comprobamos si gana la máquina
     else if (
-        (turnoMaquina === "Piedra" && turnoJugador === "Lagarto")) {
-        document.getElementById('resultado2').innerText = turnoMaquina + piedra + turnoJugador + ganaMaquina;
-        document.getElementById('resultado2').className = 'resultadoPerdedor';
-
-    } else if (
-        (turnoMaquina === "Piedra" && turnoJugador === "Tijera")) {
-        document.getElementById('resultado2').innerText = turnoMaquina + piedra + turnoJugador + ganaMaquina;
+        (turnoJugador === "Piedra" && turnoMaquina === "Lagarto")) {
+        document.getElementById('resultado2').innerText = turnoJugador + piedra + turnoMaquina + ganaJugador;
+        document.getElementById('resultado2').className = 'resultadoGanador';
+    }
+    else if (
+        (turnoJugador === "Piedra" && turnoMaquina === "Spock")) {
+        document.getElementById('resultado2').innerText = turnoMaquina + spock2 + turnoJugador + ganaMaquina;
         document.getElementById('resultado2').className = 'resultadoPerdedor';
     }
-    // Comprobamos si gana la máquina
-    else if (
-        (turnoMaquina === "Papel" && turnoJugador === "Piedra")) {
-        document.getElementById('resultado2').innerText = turnoMaquina + papel + turnoJugador + ganaMaquina;
-        document.getElementById('resultado2').className = 'resultadoPerdedor';
-    } else if (
-        (turnoMaquina === "Papel" && turnoJugador === "Spock")) {
-        document.getElementById('resultado2').innerText = turnoMaquina + papel2 + turnoJugador + ganaMaquina;
-        document.getElementById('resultado2').className = 'resultadoPerdedor';
-    }
-    // Comprobamos si gana el jugador
-    else if (
+    // Comprobamos sacando la Papel
+    if (
         (turnoJugador === "Papel" && turnoMaquina === "Piedra")) {
-        document.getElementById('resultado2').innerText = turnoJugador + papel + turnoMaquina + ganaJugador;
+        document.getElementById('resultado2').innerText = turnoJugador + papel1 + turnoMaquina + ganaJugador;
         document.getElementById('resultado2').className = 'resultadoGanador';
     } else if (
+        (turnoJugador === "Papel" && turnoMaquina === "Tijera")) {
+        document.getElementById('resultado2').innerText = turnoMaquina + tijera + turnoJugador + ganaMaquina;
+        document.getElementById('resultado2').className = 'resultadoPerdedor';
+    }
+    else if (
+        (turnoJugador === "Papel" && turnoMaquina === "Lagarto")) {
+        document.getElementById('resultado2').innerText = turnoMaquina + lagarto + turnoJugador + ganaMaquina;
+        document.getElementById('resultado2').className = 'resultadoPerdedor';
+    }
+    else if (
         (turnoJugador === "Papel" && turnoMaquina === "Spock")) {
         document.getElementById('resultado2').innerText = turnoJugador + papel2 + turnoMaquina + ganaJugador;
         document.getElementById('resultado2').className = 'resultadoGanador';
     }
-    // Comprobamos si gana el jugador
-    else if (
+    // Comprobamos sacando la Tijera
+    if (
+        (turnoJugador === "Tijera" && turnoMaquina === "Piedra")) {
+        document.getElementById('resultado2').innerText = turnoMaquina + piedra + turnoJugador + ganaMaquina;
+        document.getElementById('resultado2').className = 'resultadoPerdedor';
+    } else if (
         (turnoJugador === "Tijera" && turnoMaquina === "Papel")) {
         document.getElementById('resultado2').innerText = turnoJugador + tijera + turnoMaquina + ganaJugador;
         document.getElementById('resultado2').className = 'resultadoGanador';
-    } else if (
+    }
+    else if (
         (turnoJugador === "Tijera" && turnoMaquina === "Lagarto")) {
         document.getElementById('resultado2').innerText = turnoJugador + tijera2 + turnoMaquina + ganaJugador;
         document.getElementById('resultado2').className = 'resultadoGanador';
     }
-    // Comprobamos si gana la máquina
     else if (
-        (turnoMaquina === "Tijera" && turnoJugador === "Papel")) {
-        document.getElementById('resultado2').innerText = turnoMaquina + tijera + turnoJugador + ganaMaquina;
-        document.getElementById('resultado2').className = 'resultadoPerdedor';
-    } else if (
-        (turnoMaquina === "Tijera" && turnoJugador === "Lagarto")) {
-        document.getElementById('resultado2').innerText = turnoMaquina + tijera2 + turnoJugador + ganaMaquina;
+        (turnoJugador === "Tijera" && turnoMaquina === "Spock")) {
+        document.getElementById('resultado2').innerText = turnoMaquina + spock + turnoJugador + ganaMaquina;
         document.getElementById('resultado2').className = 'resultadoPerdedor';
     }
-    // Comprobamos si gana el jugador
-    else if (
+    // Comprobamos sacando la Lagarto
+    if (
+        (turnoJugador === "Lagarto" && turnoMaquina === "Piedra")) {
+        document.getElementById('resultado2').innerText = turnoMaquina + piedra + turnoJugador + ganaMaquina;
+        document.getElementById('resultado2').className = 'resultadoPerdedor';
+    } else if (
         (turnoJugador === "Lagarto" && turnoMaquina === "Papel")) {
         document.getElementById('resultado2').innerText = turnoJugador + lagarto + turnoMaquina + ganaJugador;
         document.getElementById('resultado2').className = 'resultadoGanador';
-    } else if (
+    }
+    else if (
+        (turnoJugador === "Lagarto" && turnoMaquina === "Tijera")) {
+        document.getElementById('resultado2').innerText = turnoMaquina + tijera2 + turnoJugador + ganaMaquina;
+        document.getElementById('resultado2').className = 'resultadoPerdedor';
+    }
+    else if (
         (turnoJugador === "Lagarto" && turnoMaquina === "Spock")) {
         document.getElementById('resultado2').innerText = turnoJugador + lagarto2 + turnoMaquina + ganaJugador;
         document.getElementById('resultado2').className = 'resultadoGanador';
     }
-    // Comprobamos si gana la máquina
-    else if (
-        (turnoMaquina === "Lagarto" && turnoJugador === "Papel")) {
-        document.getElementById('resultado2').innerText = turnoMaquina + lagarto + turnoJugador + ganaMaquina;
-        document.getElementById('resultado2').className = 'resultadoPerdedor';
-    } else if (
-        (turnoMaquina === "Lagarto" && turnoJugador === "Spock")) {
-        document.getElementById('resultado2').innerText = turnoMaquina + lagarto2 + turnoJugador + ganaMaquina;
-        document.getElementById('resultado2').className = 'resultadoPerdedor';
-    }
-    // Comprobamos si gana el jugador 
-    else if (
-        (turnoJugador === "Spock" && turnoMaquina === "Tijera")) {
-        document.getElementById('resultado2').innerText = turnoJugador + spock + turnoMaquina + ganaJugador;
-        document.getElementById('resultado2').className = 'resultadoGanador';
-    } else if (
+
+    // Comprobamos sacando  Spock
+    if (
         (turnoJugador === "Spock" && turnoMaquina === "Piedra")) {
         document.getElementById('resultado2').innerText = turnoJugador + spock2 + turnoMaquina + ganaJugador;
         document.getElementById('resultado2').className = 'resultadoGanador';
     }
-    // Comprobamos si gana la máquina
     else if (
-        (turnoMaquina === "Spock" && turnoJugador === "Tijera")) {
-        document.getElementById('resultado2').innerText = turnoMaquina + spock + turnoJugador + ganaMaquina;
-        document.getElementById('resultado2').className = 'resultadoPerdedor';
-    } else if (
-        (turnoMaquina === "Spock" && turnoJugador === "Piedra")) {
-        document.getElementById('resultado2').innerText = turnoMaquina + spock2 + turnoJugador + ganaMaquina;
+        (turnoJugador === "Spock" && turnoMaquina === "Papel")) {
+        document.getElementById('resultado2').innerText = turnoMaquina + papel2 + turnoJugador + ganaMaquina;
         document.getElementById('resultado2').className = 'resultadoPerdedor';
     }
+    else if (
+        (turnoJugador === "Spock" && turnoMaquina === "Tijera")) {
+        document.getElementById('resultado2').innerText = turnoJugador + spock + turnoMaquina + ganaJugador;
+        document.getElementById('resultado2').className = 'resultadoGanador';
+    }
+    else if (
+        (turnoJugador === "Spock" && turnoMaquina === "Lagarto")) {
+        document.getElementById('resultado2').innerText = turnoMaquina + lagarto2 + turnoJugador + ganaMaquina;
+        document.getElementById('resultado2').className = 'resultadoPerdedor';
+    }
+
+
     // Comprobamos si las dos variables son iguales para saber si es un empate o no 
     else if (turnoJugador === turnoMaquina) {
         document.getElementById('resultado2').innerText = 'Es un empate';
